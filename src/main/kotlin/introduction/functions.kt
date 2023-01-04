0b8a88eccfd6f3d4f8d05b4b10326c0a2ee9f293
GreatGrant <greatgrant116@gmail.com>
2023-01-04 17:38:34 +0100

    add functions.kt

---
+++ src/main/kotlin/introduction/functions.kt
@@ -0,0 +1,21 @@
+package introduction
+
+import java.util.Calendar
+
+fun main(args: Array<String>){
+    dayOfTheWeek()
+}
+
+fun dayOfTheWeek() {
+
+    val dayOfTheWeek = when(Calendar.getInstance().get(Calendar.DAY_OF_WEEK)){
+        1 -> "Sunday"
+        2 -> "Monday"
+        3 -> "Tuesday"
+        4 -> "Wednesday"
+        5 -> "Thursday"
+        6 -> "Friday"
+        else -> {"Saturday"}
+    }
+    print(dayOfTheWeek)
+}
