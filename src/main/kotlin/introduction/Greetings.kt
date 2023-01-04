7b211bc75031b5225275baf7185e9f7a80cf73db
GreatGrant <greatgrant116@gmail.com>
2023-01-04 18:20:15 +0100

    update Greetings.kt file

--- src/main/kotlin/introduction/Greetings.kt
+++ src/main/kotlin/introduction/Greetings.kt
@@ -1,5 +1,18 @@
 package introduction
+import java.time.LocalTime
 
-fun main(){
+fun main(args: Array<String>){
+    val time = LocalTime.now().hour
+    println(greet(time))
+}
 
+/**
+ * Create a function that takes an argument representing the time in 24-hour format
+ * (values between and including 0 -> 23).
+ * check if the time is before midday (<12),
+ * then print "Good morning, Kotlin"; otherwise, print "Good night, Kotlin".
+ */
+
+fun greet(hour: Int): String{
+    return  if (hour < 12) "Good Morning Kotlin" else "Good night, Kotlin"
 }
