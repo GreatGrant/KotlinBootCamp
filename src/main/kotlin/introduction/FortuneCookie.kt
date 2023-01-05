ae79f767a1efd983d2e10ad442a98e757ab4fac3
GreatGrant <greatgrant116@gmail.com>
2023-01-05 04:31:03 +0100

    added fortune cookie code

--- src/main/kotlin/introduction/FortuneCookie.kt
+++ src/main/kotlin/introduction/FortuneCookie.kt
@@ -1,2 +1,23 @@
 package introduction
 
+fun main(){
+    print( "Your fortune is: ${getFortuneCookie()}")
+}
+
+fun getFortuneCookie() : String{
+
+    var fortunes = arrayListOf<String>(
+        "You will have a great day!",
+        "Things will go well for you today.",
+        "Enjoy a wonderful day of success.",
+        "Be humble and all will turn out well.",
+        "Today is a good day for exercising restraint.",
+        "Take it easy and enjoy life!",
+        "Treasure your friends because they are your greatest fortune."
+    )
+
+    print("Enter your birthday\n")
+    val birthday = readln()?.toIntOrNull()?: 1
+    return fortunes[birthday.rem(fortunes.size)]
+
+}
