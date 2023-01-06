4a5bc4ba55c5ffd211c91629adcf15e8e346b1aa
GreatGrant <greatgrant116@gmail.com>
2023-01-06 14:30:44 +0100

    update RandomDay.kt

--- src/main/kotlin/introduction/RandomDay.kt
+++ src/main/kotlin/introduction/RandomDay.kt
@@ -6,6 +6,7 @@ fun main(){
     val day = getFeedingDate()
     val food = getFishFood(day)
     println("Feed the fish $food on $day")
+    if (shouldChangeWater(day)) println("Change water today")
 }
 
 fun getFishFood(day: String): String {
@@ -18,6 +19,11 @@ fun getFishFood(day: String): String {
     }
 }
 
+fun shouldChangeWater(day: String,
+                      temperature: Int = 22,
+                      dirty: Int = 20 ): Boolean {
+    return true
+}
 fun getFeedingDate(): String{
     val week = listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
     return week[Random().nextInt(5)]
