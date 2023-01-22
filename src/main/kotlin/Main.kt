a1bc6e6b1039593f008d10c9326b5d2086806932
GreatGrant <greatgrant116@gmail.com>
2023-01-22 07:13:35 -0800

    add constructor to Aquarium class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,12 +2,15 @@ import OOP.Aquarium
 import OOP.SimpleSpice
 
 fun main(args: Array<String>) {
-    var myAquarium = Aquarium()
+
 
     var curry = SimpleSpice()
     curry.name = "Curry"
     curry.spiciness = "hot"
+
     println("The spice name is ${curry.name}, and the spiciness is ${curry.heat}")
+
+    var myAquarium = Aquarium()
     println("Length of aquarium is ${myAquarium.length},\n height is ${myAquarium.height}" +
             " and width is ${myAquarium.width}")
 }
--- src/main/kotlin/OOP/Aquarium.kt
+++ src/main/kotlin/OOP/Aquarium.kt
@@ -1,11 +1,14 @@
 package OOP
 
-class Aquarium {
-    var width = 23
-    var height = 20
-    var length = 15
+class Aquarium(
+    var width: Int = 23,
+    var height: Int = 20,
+    var length: Int = 15
+) {
 
     var volume: Int
         get() = width.times(height).times(length) / 1000
-        set(value) {height = value.times(1000) / width.times(length) }
+        set(value) {
+            height = value.times(1000) / width.times(length)
+        }
 }
