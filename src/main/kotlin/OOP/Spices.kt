62c3b5249f01f8ce79462998303635b8bf12aaab
GreatGrant <greatgrant116@gmail.com>
2023-01-23 01:41:51 -0800

    Create and implement interface Grinder and abstract class Spices in Spices.kt

---
+++ src/main/kotlin/OOP/Spices.kt
@@ -0,0 +1,41 @@
+package OOP
+
+abstract class Spices(
+    private var name: String,
+    private var spiciness: String = "mild"
+) {
+
+    abstract fun prepareSpice()
+    private val heat: Int
+        get() = when (spiciness) {
+            "mild" -> 5
+            "Medium" -> 10
+            "Spicy" -> 15
+            "Very spicy" -> 20
+            else -> 0
+        }
+
+    init {
+        println("$name is $spiciness with output: $heat")
+    }
+}
+
+interface Grinder{
+    fun grind()
+}
+
+
+
+class Curry(private var name: String = "Curry", spiciness: String="mild"): Spices(name), Grinder{
+    override fun prepareSpice() {
+        println("Preparing $name")
+    }
+
+    override fun grind() {
+        println("Grinding $name")
+    }
+
+}
+fun main() {
+
+}
