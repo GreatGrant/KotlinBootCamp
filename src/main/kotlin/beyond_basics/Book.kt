de8cfdbfd6c13b324c777dc1bc6c3043eb892a8b
GreatGrant <greatgrant116@gmail.com>
2023-01-23 09:51:33 -0800

    Add code to Book.kt for collection handling

--- src/main/kotlin/beyond_basics/Book.kt
+++ src/main/kotlin/beyond_basics/Book.kt
@@ -12,8 +12,35 @@ data class Book(var title: String, var author: String, var year: Int)
 fun retrieveBookDetails(book: Book): Pair<String, String> = Pair(book.title, book.author)
 fun retrieveAllBookDetails(book: Book): Triple<String, String, Int> = Triple(book.title, book.author, book.year)
 
-fun main(){
-    val book = Book(title = "Harry Porter", author = "J.K Rowling", 2002)
-    var (title, author, year) = retrieveAllBookDetails(book)
+fun main() {
+    val purpleHibiscus = Book(title = "Purple Hibiscus", author = "Chimamanda Adichie", 2007)
+    val halfOfAYellowSun = Book(title = "Half of a yellow sun", author = "Chimamanda Adichie", 2007)
+    val harryPorter = Book(title = "Harry Porter", author = "J.K Rowling", 2002)
+    var romeoAndJuliet = Book(title = "Romeo and Juliet", author = "Williams Shakespeare", 1900)
+    var hamlet = Book(title = "Hamlet", author = "Williams Shakespeare", 1902)
+    var macBeth = Book(title = "Macbeth", author = "Williams Shakespeare", 1902)
+
+//    String deconstruction
+    var (title, author, year) = retrieveAllBookDetails(harryPorter)
     println("Author of $title is $author. It was written in $year")
+
+    var books = listOf(purpleHibiscus, harryPorter, halfOfAYellowSun, macBeth, hamlet, romeoAndJuliet)
+
+    var booksByShakespeare = mutableListOf<String>()
+    books.filter { book -> book.author == "Williams Shakespeare" }
+        .forEach { book -> booksByShakespeare.add(book.title) }
+
+
+    var library = mapOf("Williams Shakespeare" to booksByShakespeare)
+// Checks if hamlet is contained in library
+    var isHamlet = library.any { it.value.contains("Hamlet") }
+    println(isHamlet)
+
+    val moreBooks = mutableMapOf<String, String>()
+    moreBooks["The Great Gatsby"] = "F. Scott Fitzgerald"
+
+    val newBook = "Pride and Prejudice"
+    val bookAuthor = moreBooks.getOrPut(newBook) { "Jane Austen" }
+
+
 }
